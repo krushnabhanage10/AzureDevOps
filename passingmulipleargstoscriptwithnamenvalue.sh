de4b839198bc92a4564933d767a6ba6f1e75caa9
Krushna Bhanage <46320727+krushnabhanage10@users.noreply.github.com>
2023-03-09 18:00:19 +0530

    Create passingmulipleargstoscriptwithnamenvalue.sh

---
+++ passingmulipleargstoscriptwithnamenvalue.sh
@@ -0,0 +1,12 @@
+
+# paas as varname="value" var2name="calue" var3name="value"
+
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+done
